efbbc78824631c273073c7e6deec522c7d6cd541
Barman <dbarman2@dxc.com>
2019-12-08 20:05:24 +0530

    localStorage added

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,11 +1,18 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import uuid from "uuid/v1";
 
 export const TodoContext = createContext();
 
 const TodoContextProvider = props => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    const localTodos = localStorage.getItem("todos");
+    console.log(localTodos);
+    return localTodos ? JSON.parse(localTodos) : [];
+  });
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const addTodo = todo => {
     setTodos([...todos, { todo, id: uuid() }]);
   };
